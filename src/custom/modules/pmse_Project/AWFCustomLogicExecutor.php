dbf24746608318616e6734b20a38f2b46aa03653
Hector Rios <hrios@sugarcrm.com>
2020-06-24 10:42:35 +0200

    new interface that all custom logic implementations need to implement

---
+++ src/custom/modules/pmse_Project/AWFCustomLogicExecutor.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Sugarcrm\Sugarcrm\custom\modules\pmse_Project;
+
+
+use PMSERunnable;
+
+interface AWFCustomLogicExecutor extends PMSERunnable
+{
+    public function getModules();
+}
