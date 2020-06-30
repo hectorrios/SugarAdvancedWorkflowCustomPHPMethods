a3e2b5e17ade2bcc93ec04c4cd07693518e16ffb
Hector Rios <hrios@sugarcrm.com>
2020-06-30 15:07:14 +0200

    added the Registry as a dependency on the constructor in order to delegate the look-ups to the Registry

--- src/custom/modules/pmse_Project/AWFCustomAction.php
+++ src/custom/modules/pmse_Project/AWFCustomAction.php
@@ -6,28 +6,44 @@
 // 2017-04-27
 // Tested on Sugar 7.8.2.0
 
+use Sugarcrm\Sugarcrm\custom\modules\pmse_Project\AWFCustomActionRegistry;
+use Sugarcrm\Sugarcrm\custom\modules\pmse_Project\AWFCustomLogicExecutor;
+use Sugarcrm\Sugarcrm\DependencyInjection\Container;
+
 class AWFCustomAction
 {
-    public $availableMethods = array();
     public $previousUser;
 
-    public function methodExists($method, $module)
+    /* @var $executorRegistry AWFCustomActionRegistry */
+    private $executorRegistry;
+
+    /**
+     * AWFCustomAction constructor.
+     * @param AWFCustomActionRegistry $executorRegistry
+     */
+    public function __construct(AWFCustomActionRegistry $executorRegistry)
     {
-        if (!empty($method) && !empty($module) && !empty($this->availableMethods[$module])) {
-            if (in_array($method, $this->availableMethods[$module]) && method_exists($this, $method)) {
-                return true;
-            }
-        }
-        return false;
+        $this->executorRegistry = $executorRegistry;
     }
 
+//    public function methodExists($method, $module)
+//    {
+//        if (!empty($method) && !empty($module) && !empty($this->availableMethods[$module])) {
+//            if (in_array($method, $this->availableMethods[$module]) && method_exists($this, $method)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+
+    /**
+     * Fetch the list of module names that have
+     * custom action executors registered against them
+     */
     public function getAvailableModulesApis()
     {
-        if (!empty($this->availableMethods)) {
-            return array_keys($this->availableMethods);
-        }
-    
-        return array();
+        //Use the Registry to fetch the list of modules
+        return $this->executorRegistry->getAvailableModules();
     }
 
     public function getAvailableApis($module = null)
@@ -41,20 +57,17 @@ class AWFCustomAction
             'result' => array(),
         );
 
-        if (!empty($this->availableMethods) && !empty($this->availableMethods[$module])) {
-            foreach ($this->availableMethods[$module] as $method) {
-                if (method_exists($this, $method)) {
-                    $response['result'][] = array(
-                        'value' => $method,
-                        'text' => $method,
-                    );
-                }
-            }
-    
-            return $response;            
+        //Use the registry to get the list of Executors for this module
+        $executorEntries = $this->executorRegistry->getAvailableExecutorsForModule($module);
+        foreach ($executorEntries as $executorEntry) {
+            $response["result"][] = [
+                'value' => $executorEntry["containerKey"],
+                'text' => $executorEntry["label"],
+            ];
         }
-        
-        return array('success' => false);
+
+        return $response;
+        //return array('success' => false);
     }
 
     public function impersonateUser($user)
@@ -100,28 +113,28 @@ class AWFCustomAction
         return false;
     }
 
-    public function callCustomLogic(SugarBean $b, $method, $additional_info)
-    {
-        if ($b instanceof SugarBean) {
+//    public function callCustomLogic(SugarBean $b, $method, $additional_info)
+//    {
+//        if ($b instanceof SugarBean) {
 
             // retrieve the original user that initiated the process, if there was a timer to push the action in the background
             // right now there seem to be no way to differentiate if a process is real time if it was sent in the background
             // logic will have to apply to the executing logic, based on the method called
 
-            $override_user = null;
-            if (!empty($additional_info['flowData']['cas_id'])) {
-                $override_user = $this->retrieveOriginalUserForProcess($additional_info['flowData']['cas_id']);
-            }
-
-            if ($this->methodExists($method, $b->module_name)) {
-                try {
-                    return call_user_func_array(array($this, $method), array($b, $override_user, $additional_info));
-                } catch (Exception $e) {
-                    return false;
-                }
-            }
-        }
-
-        return false;
-    }
+//            $override_user = null;
+//            if (!empty($additional_info['flowData']['cas_id'])) {
+//                $override_user = $this->retrieveOriginalUserForProcess($additional_info['flowData']['cas_id']);
+//            }
+//
+//            if ($this->methodExists($method, $b->module_name)) {
+//                try {
+//                    return call_user_func_array(array($this, $method), array($b, $override_user, $additional_info));
+//                } catch (Exception $e) {
+//                    return false;
+//                }
+//            }
+//        }
+//
+//        return false;
+//    }
 }
