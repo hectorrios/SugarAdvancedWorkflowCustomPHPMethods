5686a26df712f9012f2e84f37a0073ece47a2379
Hector Rios <hrios@sugarcrm.com>
2020-07-01 14:51:40 +0200

    add missing imports for the Container and ContainerInterface

--- src/custom/logichooks/application/RegisterTheStarterCustomAction.php
+++ src/custom/logichooks/application/RegisterTheStarterCustomAction.php
@@ -4,7 +4,9 @@
 namespace Sugarcrm\Sugarcrm\custom\logichooks\application;
 
 
+use Psr\Container\ContainerInterface;
 use Sugarcrm\Sugarcrm\custom\inc\awfactions\StarterCustomAction;
+use Sugarcrm\Sugarcrm\DependencyInjection\Container;
 
 class RegisterTheStarterCustomAction
 {
