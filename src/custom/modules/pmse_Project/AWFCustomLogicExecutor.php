bce6c78251c621cb9fe1f373a087f68acbc4e059
Hector Rios <hrios@sugarcrm.com>
2020-07-23 11:39:11 +0200

    add documentation to the interface methods

--- src/custom/modules/pmse_Project/AWFCustomLogicExecutor.php
+++ src/custom/modules/pmse_Project/AWFCustomLogicExecutor.php
@@ -8,7 +8,19 @@ use PMSERunnable;
 
 interface AWFCustomLogicExecutor extends PMSERunnable
 {
+    /**
+     * The modules that are supported for this Executor. The concrete Executor
+     * will be made visible to a BPM definition if the BPM Module is part of this list.
+     *
+     * @return array of module names that are supported by the concrete Executor
+     */
     public function getModules();
 
+    /**
+     * The readable name for this Executor that will be displayed to the BPM definition designer when configuring
+     * the @see \PMSECallCustomLogic element.
+     *
+     * @return string
+     */
     public function getLabelName();
 }
