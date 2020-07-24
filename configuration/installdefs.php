6d2ddd20456cb27b38ac9bc25b5b3957b022ccab
Hector Rios <hrios@sugarcrm.com>
2020-07-24 16:53:42 +0200

    add installdef keys for the MLP packaging

--- configuration/installdefs.php
+++ configuration/installdefs.php
@@ -1,2 +1,27 @@
 <?php
 $installdefs['post_execute'] = array('<basepath>/scripts/post_execute.php');
+$installdefs['jsgroups'] = array(
+    array(
+        'from' => '<basepath>/custom/Extension/application/Ext/JSGroupings/pmsegrouping.php',
+        'to_module' => 'application',
+    ),
+);
+
+$installdefs['language'] = array(
+    array(
+        'from' => '<basepath>/custom/Extension/application/Ext/Language/en_us.custom_workflow_logic.php',
+        'to_module' => 'application',
+        'language' => 'en_us'
+    ),
+);
+
+$installdefs['hookdefs'] = array(
+    array(
+        'from' => '<basepath>/custom/Extension/application/Ext/LogicHooks/install_AWFRegistry_afterEntryPoint.php',
+        'to_module' => 'application',
+    ),
+    array(
+        'from' => '<basepath>/custom/Extension/application/Ext/LogicHooks/install_StarterCustomAction_afterEntryPoint.php',
+        'to_module' => 'application',
+    ),
+);
